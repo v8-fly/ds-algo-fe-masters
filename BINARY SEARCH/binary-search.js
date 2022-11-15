ff1b1b541b42d71047cb67183630a422f63348fa
v8-fly <98163507+v8-fly@users.noreply.github.com>
2022-11-15 08:24:21 +0530

    init

---
+++ BINARY SEARCH/binary-search.js
@@ -0,0 +1,17 @@
+const headerName = "BINARY SEARCH"
+const title = document.getElementById("title")
+title.innerHTML = `<h1>${headerName}</h1>`
+
+/*
+
+The array should be ordered
+comlexity => O(logn)
+
+Remember if we perform halfing in solution
+its either logn or nLogn 
+
+*/
+
+const binarySearch = (arr, toFind) => {}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 15))
